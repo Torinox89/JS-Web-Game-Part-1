f9eca2d637218f54538060014b8594b2f8ac6765
Adrian Mitre <mitrehealthcare@gmail.com>
2022-06-17 21:51:25 -0700

    create character and add event listener

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+function newImage(url, left, bottom){
+
+let character = document.createElement('img')
+character.src = url
+character.style.position = 'fixed'
+character.style.left = left + 'px'
+character.style.bottom = bottom + 'px'
+document.body.append(character)
+
+return character
+}
+
+function newImage(url, left, bottom){
+  let character =  newImage(url, left, bottom)
+  
+  object.addEventListener('dblclick', () => {
+    object.remove()
+})
+
+}
+
+newImage('assets/green-character.gif', 100, 100)
+
+
