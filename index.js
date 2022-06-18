3d284ccd4729d433787ce1df1cfe269dc9957fbd
Adrian Mitre <mitrehealthcare@gmail.com>
2022-06-17 23:45:03 -0700

    doble click

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 
-function newImage(url, left, bottom){
 
-let character = document.createElement('img')
-character.src = url
-character.style.position = 'fixed'
-character.style.left = left + 'px'
-character.style.bottom = bottom + 'px'
-document.body.append(character)
 
-return character
-}
+
 
 function newImage(url, left, bottom){
-  let character =  newImage(url, left, bottom)
-  
-  object.addEventListener('dblclick', () => {
-    object.remove()
-})
-
+    let character = document.createElement('img')
+    character.src = url
+    character.style.position = 'fixed'
+    character.style.left = left + 'px'
+    character.style.bottom = bottom + 'px'
+    document.body.append(character)
+    return character
 }
+
+function newItem(url, left, bottom){
+    let character = newImage(url, left, bottom)
+
+    character.addEventListener('dblclick', () => {
+        character.remove()
+    })
+}
+
 
 newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
 
+
+newItem('assets/sword.png', 500, 405)
+
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
 
